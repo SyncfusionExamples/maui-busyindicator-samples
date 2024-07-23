2ab35e99a6c85b879bfcb4622687dd545d1afba4
Brundha <brundha.sf3969@gmail.com>
2024-07-23 15:33:58 +0530

    Added busyIndicator sample

---
+++ BusyIndicatorOverlayFill/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BusyIndicatorOverlayFill
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
